d8591039d87a79d46e84d64bb60fefd03531a18c
jdao55 <joedao55@gmail.com>
2019-02-10 20:38:40 -0500

    updated company setup

--- init.el
+++ init.el
@@ -644,23 +644,23 @@
   (require 'lsp-ui))
 
 
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ;; Set up code completion with company
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (use-package company
-;;   :ensure t
-;;   :config
-;;   ;; Zero delay when pressing tab
-;;   (setq company-idle-delay 0)
-;;   (add-hook 'after-init-hook 'global-company-mode)
-;;   ;; remove unused backends
-;;   (setq company-backends (delete 'company-semantic company-backends))
-;;   (setq company-backends (delete 'company-eclim company-backends))
-;;   (setq company-backends (delete 'company-xcode company-backends))
-;;   (setq company-backends (delete 'company-clang company-backends))
-;;   (setq company-backends (delete 'company-bbdb company-backends))
-;;   (setq company-backends (delete 'company-oddmuse company-backends))
-;;   )
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Set up code completion with company
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package company
+  :ensure t
+  :config
+  ;; Zero delay when pressing tab
+  (setq company-idle-delay 0)
+  (add-hook 'after-init-hook 'global-company-mode)
+  ;; remove unused backends
+  ;; (setq company-backends (delete 'company-semantic company-backends))
+  ;; (setq company-backends (delete 'company-eclim company-backends))
+  ;; (setq company-backends (delete 'company-xcode company-backends))
+  ;; (setq company-backends (delete 'company-clang company-backends))
+  ;; (setq company-backends (delete 'company-bbdb company-backends))
+  ;; (setq company-backends (delete 'company-oddmuse company-backends))
+  )
 
 ;; ;; Setup loading company-jedi for python completion
 ;; ;; This requines running jedi:install-server the first time
